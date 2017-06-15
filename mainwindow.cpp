be056fb3ad3bb39000a0408e2304ab649ac34748
Mousa Moradi <mousamk@gmail.com>
2017-06-16 03:32:33 +0430

    Reformating if brackets.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -55,8 +55,7 @@ void MainWindow::initPomodoro()
 
 void MainWindow::mousePressEvent(QMouseEvent* event)
 {
-    if(event->button() == Qt::LeftButton)
-    {
+    if (event->button() == Qt::LeftButton) {
         mMoving = true;
         mLastMousePosition = event->pos();
     }
@@ -64,16 +63,12 @@ void MainWindow::mousePressEvent(QMouseEvent* event)
 
 void MainWindow::mouseMoveEvent(QMouseEvent* event)
 {
-    if( event->buttons().testFlag(Qt::LeftButton) && mMoving)
-    {
+    if (event->buttons().testFlag(Qt::LeftButton) && mMoving)
         this->move(this->pos() + (event->pos() - mLastMousePosition));
-    }
 }
 
 void MainWindow::mouseReleaseEvent(QMouseEvent* event)
 {
-    if(event->button() == Qt::LeftButton)
-    {
+    if (event->button() == Qt::LeftButton)
         mMoving = false;
-    }
 }
